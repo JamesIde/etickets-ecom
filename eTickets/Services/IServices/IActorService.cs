99d8703d5380b9b88de14f14569a6f0048dbccb1
James <james.ide775@gmail.com>
2021-12-21 15:07:06 +1030

    added navbar + recovered files bc VS wiped

---
+++ eTickets/Services/IServices/IActorService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using eTickets.Models;
+namespace eTickets.Services.IServices
+{
+    public interface IActorService
+    {
+        public Task<IEnumerable<Actor>> GetActors();
+
+    }
+}
